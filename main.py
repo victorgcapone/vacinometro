71434ba5a45bcb4b087f513ce0f8aef7cf9bb87a
Victor Capone <v.caponemeirelles@gmail.com>
2021-01-20 16:34:49 -0300

    Commit inicial

---
+++ main.py
@@ -0,0 +1,36 @@
+from selenium import webdriver
+import pandas as pd
+import sched, time
+
+URL = "https://www.saopaulo.sp.gov.br/"
+VACINOMETRO_CLASS_NAME="tableauPlaceholder"
+DATE_CLASS_NAME="data-atualizacao-vac"
+
+vacinados = pd.read_csv("vacinados.csv")
+s = sched.scheduler(time.time, time.sleep)
+
+def get_vacinados(driver):
+    element = driver.find_element_by_class_name(VACINOMETRO_CLASS_NAME)
+    text = element.text.replace(".", "")
+    return int(text)
+
+
+def get_data(driver):
+    element = driver.find_element_by_class_name(DATE_CLASS_NAME)
+    text = element.text.split("em")[1].strip()
+    return text
+
+
+def fetch_data(vacinados):
+    driver = webdriver.Chrome()
+    driver.get(URL)
+    new_data = {"Data": [get_data(driver)], "Vacinados":[get_vacinados(driver)]}
+    driver.close()
+    print(new_data)
+    vacinados = vacinados.append(pd.DataFrame(new_data), ignore_index=True)
+    vacinados.to_csv("vacinados.csv", index=False)
+    s.enter(300, 1, fetch_data, (vacinados,))
+
+
+s.enter(300, 1, fetch_data, (vacinados,))
+s.run()
